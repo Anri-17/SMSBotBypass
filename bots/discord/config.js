85d535f511b7d23f90309b387860b4fa07031091
Anri-17 <119749071+Anri-17@users.noreply.github.com>
2025-01-31 16:54:51 +0400

    Update config.js

--- bots/discord/config.js
+++ bots/discord/config.js
@@ -2,9 +2,9 @@ module.exports = {
     apiurl: 'http://localhost:1337',
     apipassword: '',
 
-    discordtoken: '',
-    discordprefix: '!',
-    secretpassword: 'ChAnGeThIsWiThYoUrPaSsWoRdNoW!',
+    discordtoken: 'MTMzNDg2OTIwOTYyODU0NTA3Nw.GEvq-3.uWBNvObYsdN3OYM1URdISE1yDJGbV9nD259LNA',
+    discordprefix: '/',
+    secretpassword: 'Anex17!',
     botuser_rolename: 'Bot User',
     admin_rolename: 'Admin'
 };
